e148e701582350d7ef12cb13930637caf58c9698
Adam Jamiu <adamjamiu98@gmail.com>
2022-04-27 04:07:31 +0100

    styles updates

--- src/components/VideoLogo/VideoLogo.js
+++ src/components/VideoLogo/VideoLogo.js
@@ -1,52 +1,53 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import VideoMask from "../../assets/images/VideoMask";
+// import { makeStyles } from "@material-ui/core/styles";
+// import VideoMask from "../../assets/images/VideoMask";
 
-const VideoLogo = (props) => {
-    const classes = useStyles();
+const VideoLogo = () => {
+    // const classes = useStyles();
     return (
-        <div className={classes.wrapper} {...props}>
-          <VideoMask />
+        <div>
+          {/* <VideoMask /> */}
+          <h1>ADAM JAMIU</h1>
         </div>
     );
 };
 
-const useStyles = makeStyles((theme) => ({
-    wrapper: {
-        overflow:"hidden",
-        position: "relative",
-        width: "300px",
-        height: "68px",
-        [theme.breakpoints.down("xs")]: {
-            width: "230px",
-            height: "53px",
-        },
-    },
-    mask: {
-        zIndex: 1,
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        width: "300px",
-        [theme.breakpoints.down("xs")]: {
-            width: "230px",
-            height: "52px",
-        },
-    },
-    video: {
-        zIndex: 0,
-        position: "absolute",
-        top: 0,
-        left: "1px",
-        right: "1px",
-        width: "298px",
-        height: "68px",
-        [theme.breakpoints.down("xs")]: {
-            width: "228px",
-            height: "52px",
-        },
-    },
-}));
+// const useStyles = makeStyles((theme) => ({
+//     wrapper: {
+//         overflow:"hidden",
+//         position: "relative",
+//         width: "300px",
+//         height: "68px",
+//         [theme.breakpoints.down("xs")]: {
+//             width: "230px",
+//             height: "53px",
+//         },
+//     },
+//     mask: {
+//         zIndex: 1,
+//         position: "absolute",
+//         top: 0,
+//         left: 0,
+//         right: 0,
+//         width: "300px",
+//         [theme.breakpoints.down("xs")]: {
+//             width: "230px",
+//             height: "52px",
+//         },
+//     },
+//     video: {
+//         zIndex: 0,
+//         position: "absolute",
+//         top: 0,
+//         left: "1px",
+//         right: "1px",
+//         width: "298px",
+//         height: "68px",
+//         [theme.breakpoints.down("xs")]: {
+//             width: "228px",
+//             height: "52px",
+//         },
+//     },
+// }));
 
 export default VideoLogo;
--- src/data.js
+++ src/data.js
@@ -24,14 +24,14 @@ const projectList = [
             "https://img.freepik.com/free-vector/dark-gradient-background-with-copy-space_53876-99548.jpg?size=626&ext=jpg&ga=GA1.2.2102900112.1628985600",
         frontImage: "https://i.ibb.co/0QFY9V9/study.png",
     },
-    {
-        id: 3,
-        title: "Burpees.io",
-        technologies: ["React JS", "Node JS", "MongoDB"],
-        backgroundImage:
-            "https://media.istockphoto.com/vectors/abstract-purple-vector-background-with-stripes-vector-id972475894?k=6&m=972475894&s=612x612&w=0&h=99AirGMOb64N2-1ZSMYRjEBp2USrAdzXUGzQMh5o6Js=",
-        frontImage: "https://themes-backend.material-ui.com/wp-content/uploads/2021/05/01_preview.jpg",
-    },
+    // {
+    //     id: 3,
+    //     title: "Burpees.io",
+    //     technologies: ["React JS", "Node JS", "MongoDB"],
+    //     backgroundImage:
+    //         "https://media.istockphoto.com/vectors/abstract-purple-vector-background-with-stripes-vector-id972475894?k=6&m=972475894&s=612x612&w=0&h=99AirGMOb64N2-1ZSMYRjEBp2USrAdzXUGzQMh5o6Js=",
+    //     frontImage: "https://themes-backend.material-ui.com/wp-content/uploads/2021/05/01_preview.jpg",
+    // },
 ];
 
 const experienceList = [
